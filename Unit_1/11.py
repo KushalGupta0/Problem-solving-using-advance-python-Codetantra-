66852bbfc36734aba115ede8457feee2a42ba1b9
byteCrafted <168537654+KushalGupta0@users.noreply.github.com>
2025-06-12 23:12:25 +0530

    Update 11.py

--- Unit_1/11.py
+++ Unit_1/11.py
@@ -1,13 +1,13 @@
 class Temperature:
 
-    def __init__(self, temperature):
-        self.temperature = temperature
+	def __init__(self, temperature):
+		self.temperature = temperature
 
-    def convert_to_fahrenheit(self):
-        return list(map(lambda x: (x * 9/5) + 32, self.temperature))
-    
-celsius = input("Enter a list of temperature in celsius: ")
-celsius = celsius.split(' ')
-celsius = [int(i) for i in celsius]
+	def convert_to_fahrenhiet(self):
+		return list(map(lambda x: (x * 9/5)+ 32, self.temperature))
+
+celsius = input("Enter a list of temperatures in Celsius: ")
+celsius = celsius.split(" ")
+celsius = [float(i) for i in celsius]
 temp = Temperature(celsius)
-print("Temperature in Fahrenheit:", temp.convert_to_fahrenheit())
+print("Temperatures in Fahrenheit: ", temp.convert_to_fahrenhiet())
